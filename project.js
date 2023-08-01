7417f12c698226ba651e2b41b4d2f89dd1437561
Ashley Ken Comandao <ashleyken.professional@gmail.com>
2023-08-01 21:26:46 +0800

    Add change #2, main branch

--- project.js
+++ project.js
@@ -10,4 +10,6 @@ console.log('This change #3 from comandao_feature_practice_branching.');
 console.log();
 
 /* ==================== CODE ADDITIONS #2 ==================== */
-console.log('This change #3 from main branch.');
+console.log('This change #1 from main branch.');
+console.log('This change #2 from main branch.');
+console.log();
