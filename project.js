07a2a1d8a937b8f23aeac154298ad0f8f0b9af4d
Ashley Ken Comandao <ashleyken.professional@gmail.com>
2023-08-01 20:22:39 +0800

    Make original version of project.js.

--- project.js
+++ project.js
@@ -1,2 +1,3 @@
 /* ==================== INITIALIZATION ==================== */
-console.log();
+console.log('Initialize the local git repository.');
+console.log('Do the same on the remote repository.');
