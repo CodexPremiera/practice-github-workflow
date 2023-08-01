a9367cfac193fab74ecff671d4068ac5fc2ab0d5
Ashley Ken Comandao <ashleyken.professional@gmail.com>
2023-08-01 20:39:14 +0800

    Add change #1

--- project.js
+++ project.js
@@ -1,3 +1,7 @@
 /* ==================== INITIALIZATION ==================== */
 console.log('Initialize the local git repository.');
 console.log('Do the same on the remote repository.');
+console.log();
+
+/* ==================== CODE ADDITIONS ==================== */
+console.log('This change #1 from comandao_feature_practice_branching.');
