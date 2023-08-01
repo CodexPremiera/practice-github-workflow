c4760e268c163b45c6d80c7a322b4caef791bec3
Ashley Ken Comandao <ashleyken.professional@gmail.com>
2023-08-01 20:40:18 +0800

    Add change #3

--- project.js
+++ project.js
@@ -6,4 +6,5 @@ console.log();
 /* ==================== CODE ADDITIONS ==================== */
 console.log('This change #1 from comandao_feature_practice_branching.');
 console.log('This change #2 from comandao_feature_practice_branching.');
+console.log('This change #3 from comandao_feature_practice_branching.');
 console.log();
